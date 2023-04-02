d8e830cfcc3b6f888ac9c75f5859e03b470cf76c
lemstanislav5 <lemstanislav5@gmail.com>
2023-04-02 23:31:30 +0300

    2121

--- services/telegramBot.js
+++ services/telegramBot.js
@@ -1,4 +1,3 @@
-const process = require('process');
-console.log(process.env.TELEGRAM_API_TOKEN)
+const process = require('process');s
 const TelegramBot = require('node-telegram-bot-api');
 module.exports = new TelegramBot(process.env.TELEGRAM_API_TOKEN, {polling: true});
