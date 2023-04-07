693eef60e18471a7fdab0b7b1bd7d4271365ce6c
lemstanislav5 <lemstanislav5@gmail.com>
2023-04-07 17:16:50 +0300

    .replace('/api', '')

--- routes/mediaRoutes.js
+++ routes/mediaRoutes.js
@@ -3,13 +3,11 @@ const fs = require("fs");
 const path = require('path');
 
 module.exports = (req, res) => {
-  console.log(path.join(process.cwd(), req.originalUrl), req.originalUrl, process.cwd());
+  console.log(path.join(process.cwd(), req.originalUrl).replace('/api', ''), req.originalUrl, process.cwd());
   try {
-    if (fs.existsSync(path.join(process.cwd(), req.originalUrl))) {
-      console.log(200)
-      return res.status(200).sendFile(path.join(process.cwd(), req.originalUrl));
+    if (fs.existsSync(path.join(process.cwd(), req.originalUrl).replace('/api', ''))) {
+      return res.status(200).sendFile(path.join(process.cwd(), req.originalUrl.replace('/api', '')));
     }
-    console.log(202)
     return res.status(202).send();
   } catch(err) {
     console.error(err);
