2fe655df9a1f3e0ff9f7fa5e0d8f6d98a8abbd90
lemstanislav5 <lemstanislav5@gmail.com>
2023-04-07 16:54:48 +0300

    12121

--- routes/mediaRoutes.js
+++ routes/mediaRoutes.js
@@ -1,12 +1,17 @@
 const process = require('process');
-module.exports = (req, res) => {
-  console.log(__dirname, req.originalUrl, process.cwd())
-  try {
-    if (fs.existsSync(path.join(process.cwd(), req.originalUrl))) {
-      return res.status(200).sendFile(path.join(process.cwd(), req.originalUrl));
-    }
-    return res.status(202).send();
-  } catch(err) {
-    console.error(err);
-  }
-}
+const express = require('express'),
+      router = express.Router()
+        .route('/')
+        .get((req, res) => {
+          console.log(__dirname, req.originalUrl, process.cwd());
+          try {
+            if (fs.existsSync(path.join(process.cwd(), req.originalUrl))) {
+              return res.status(200).sendFile(path.join(process.cwd(), req.originalUrl));
+            }
+            return res.status(202).send();
+          } catch(err) {
+            console.error(err);
+          }
+        })
+
+module.exports = router;
