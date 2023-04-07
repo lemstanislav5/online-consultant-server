fde5843096befe59b6120912169c917a741f7a02
lemstanislav5 <lemstanislav5@gmail.com>
2023-04-07 16:58:02 +0300

    1221

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 let express = require('express')
   router = express.Router(),
-  mediaRoutes = require('./mediaRoutes');
+  // mediaRoutes = require('./mediaRoutes');
 
-  router.use('/media*', mediaRoutes)
+  router.use('/media*', (req, res) => {
+    console.log(__dirname, req.originalUrl, process.cwd());
+    try {
+      if (fs.existsSync(path.join(process.cwd(), req.originalUrl))) {
+        return res.status(200).sendFile(path.join(process.cwd(), req.originalUrl));
+      }
+      return res.status(202).send();
+    } catch(err) {
+      console.error(err);
+    }
+  })
   /*
     1) Добавление юзера черезщ веб интерфес с логином и паролем, а также подтверждением через емаил или телеграмм???
     Авторизация для пользователя Manager
