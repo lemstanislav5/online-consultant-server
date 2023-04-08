a02bc2cebe32af4d064b552815c06629c7f47cac
lemstanislav5 <lemstanislav5@gmail.com>
2023-04-08 21:40:11 +0300

    212

--- handlers/handlers.js
+++ handlers/handlers.js
@@ -228,6 +228,7 @@ module.exports = {
     try {
       // Получаем managerId 
       const manager = await ManagerController.get();
+      console.lof(manager)
       // Если менеджер отсуствует или не имеет доступ (не ввел пароль) отправляем уведомление
       if (manager.length === 0 || manager[0].accest === 0) {
         io.to(socket.id).emit('notification', 'Менеджер offline!');
